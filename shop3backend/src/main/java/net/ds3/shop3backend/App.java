db50fefea461587cbeb86c7fb204b4625f19362e
Code0 <franciskameni@gmail.com>
2019-05-30 10:09:24 +0200

    first commit to branch5
    new code starting from zero

---
+++ shop3backend/src/main/java/net/ds3/shop3backend/App.java
@@ -0,0 +1,13 @@
+package net.ds3.shop3backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
