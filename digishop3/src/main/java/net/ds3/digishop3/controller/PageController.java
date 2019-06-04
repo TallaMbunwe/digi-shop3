10bf0aec5a5c51eb0309e5b1a1d7b76f74d1a6e2
Eric TALLA <eric.talla@digiplusconsulting.com>
2019-06-04 13:05:58 +0100

    Successfully fixed Url not found pages bug.

--- digishop3/src/main/java/net/ds3/digishop3/controller/PageController.java
+++ digishop3/src/main/java/net/ds3/digishop3/controller/PageController.java
@@ -1,12 +1,16 @@
 package net.ds3.digishop3.controller;
 
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.ds3.digishop3.exception.ProductNotFoundException;
 import net.ds3.digishop3backend.dao.CategoryDAO;
 import net.ds3.digishop3backend.dao.ProductDAO;
 import net.ds3.digishop3backend.dto.Category;
@@ -14,6 +18,8 @@ import net.ds3.digishop3backend.dto.Product;
 
 @Controller
 public class PageController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(PageController.class);
 	
 	@Autowired
 	private CategoryDAO categoryDAO;
@@ -26,6 +32,9 @@ public class PageController {
 		
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("title", "Home");
+		
+		logger.info("Inside PageController index method - INFO");
+		logger.debug("Inside PageController index method - DEBUG");
 		
 		//passing the list of categories
 		mv.addObject("categories", categoryDAO.list());
@@ -114,11 +123,13 @@ public class PageController {
 	 * */
 	 
 	@RequestMapping(value="/show/{id}/product")
-	public ModelAndView showSingleProduct(@PathVariable int id) {
+	public ModelAndView showSingleProduct(@PathVariable int id) throws ProductNotFoundException {
 		
 		ModelAndView mv = new ModelAndView("page");
 		
 		Product product = productDAO.get(id);
+		
+		if(product == null) throw new ProductNotFoundException();
 				
 		// update the view count
 		product.setViews(product.getViews() + 1);
